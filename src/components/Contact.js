bdfabd01f2fd705c19c9213935060022b1bb563d
priyankar-laptop <priyankarkoley@gmail.com>
2023-04-04 20:28:40 +0530

    V1.0 Complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,17 @@
 import React from "react";
 
 function Contact(props) {
-  // img=""
-  // fname=""
-  // lname=""
-  // num=""
-  // mail=""
-  // website=""
+  var n='tel:+91' + props.num;
+  var m='mailto:' + props.mail;
+  var w=props.web
   return (
     <>
       <div className="contacts">
-        <img src={props.img} alt="Contact" />
-        <p class="name">{props.fname} {props.lname}</p>
-        <p class="num">{props.num}</p>
-        <p class="mail">{props.mail}</p>
-        <p class="web">{props.web}</p>
+        <img className="img card" src={props.img} alt="Contact" />
+        <p className="name card">{props.fname} {props.lname}</p>
+        <a href={n} className="num card">{props.num} </a>
+        <a href={m} className="mail card"> {props.mail} </a>
+        <a href={w} target="_blank" className="web card"> {props.web} </a>
       </div>
     </>
   );
