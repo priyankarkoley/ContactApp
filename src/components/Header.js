c8ec73ad2b249cb4890848856b7b1428511a337a
priyankar-laptop <priyankarkoley@gmail.com>
2023-04-07 10:58:45 +0530

    TW used in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header(props) {
   return (
     <>
-      <h1 className="header">{props.heading}</h1>
+      <h1 className="m-0 p-2 text-center text-[2.9rem] header bg-[#b3c99c] font-[alkatra] ">{props.heading}</h1>
     </>
   );
 }
